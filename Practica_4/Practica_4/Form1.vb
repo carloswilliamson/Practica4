dd1d94984a9589caeb32d88013b99f05ab4d3902
carlos-williamson <carlos.williamson@iest.edu.mx>
2026-03-09 08:35:13 -0600

    Reloj en Visual studio hora MX

---
+++ Practica_4/Practica_4/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim date_o = DateTime.Now.ToString("dd 'de' MMMM 'del' yyyy")
+        Dim Day_o = DateTime.Now.ToString("dddd")
+        Dim Time_o = DateTime.Now.ToString("HH:mm")
+
+        Dim Time_mod = Time_o.Split(":")
+
+        Hora.Text = Time_mod(0)
+        Minutos.Text = Time_mod(1)
+        Dia.Text = Day_o
+        Fecha.Text = date_o
+    End Sub
+End Class
